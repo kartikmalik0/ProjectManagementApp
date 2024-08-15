e96f8ea24c512336d7149b8ae90cc66020f52ba1
Kartik <kartik20044@gmail.com>
2024-08-15 08:23:43 -0300

    shadcna and layout added

---
+++ src/app/page.tsx
@@ -0,0 +1,197 @@
+// import Link from "next/link";
+
+// import { LatestPost } from "~/app/_components/post";
+// import { getServerAuthSession } from "~/server/auth";
+// import { api, HydrateClient } from "~/trpc/server";
+
+// export default async function Home() {
+//   const hello = await api.post.hello({ text: "from tRPC" });
+//   const session = await getServerAuthSession();
+
+//   void api.post.getLatest.prefetch();
+
+//   return (
+//     <HydrateClient>
+//       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+//         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+//           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+//             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+//           </h1>
+//           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+//             <Link
+//               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+//               href="https://create.t3.gg/en/usage/first-steps"
+//               target="_blank"
+//             >
+//               <h3 className="text-2xl font-bold">First Steps →</h3>
+//               <div className="text-lg">
+//                 Just the basics - Everything you need to know to set up your
+//                 database and authentication.
+//               </div>
+//             </Link>
+//             <Link
+//               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+//               href="https://create.t3.gg/en/introduction"
+//               target="_blank"
+//             >
+//               <h3 className="text-2xl font-bold">Documentation →</h3>
+//               <div className="text-lg">
+//                 Learn more about Create T3 App, the libraries it uses, and how
+//                 to deploy it.
+//               </div>
+//             </Link>
+//           </div>
+//           <div className="flex flex-col items-center gap-2">
+//             <p className="text-2xl text-white">
+//               {hello ? hello.greeting : "Loading tRPC query..."}
+//             </p>
+
+//             <div className="flex flex-col items-center justify-center gap-4">
+//               <p className="text-center text-2xl text-white">
+//                 {session && <span>Logged in as {session.user?.name}</span>}
+//               </p>
+//               <Link
+//                 href={session ? "/api/auth/signout" : "/api/auth/signin"}
+//                 className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+//               >
+//                 {session ? "Sign out" : "Sign in"}
+//               </Link>
+//             </div>
+//           </div>
+
+//           {session?.user && <LatestPost />}
+//         </div>
+//       </main>
+//     </HydrateClient>
+//   );
+// }
+
+
+
+import Link from "next/link";
+import Image from "next/image";
+import { PanelsTopLeft } from "lucide-react";
+import { ArrowRightIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Button } from "~/components/ui/button";
+import { ModeToggle } from "~/components/mode-toggle";
+
+
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="z-[50] sticky top-0 w-full bg-background/95 border-b backdrop-blur-sm dark:bg-black/[0.6] border-border/40">
+        <div className="container h-14 flex items-center">
+          <Link
+            href="/"
+            className="flex justify-start items-center hover:opacity-85 transition-opacity duration-300"
+          >
+            <PanelsTopLeft className="w-6 h-6 mr-3" />
+            <span className="font-bold">shadcn/ui sidebar</span>
+            <span className="sr-only">shadcn/ui sidebar</span>
+          </Link>
+          <nav className="ml-auto flex items-center gap-2">
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full w-8 h-8 bg-background"
+              asChild
+            >
+              <Link href="https://github.com/salimi-my/shadcn-ui-sidebar">
+                <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
+              </Link>
+            </Button>
+            <ModeToggle />
+          </nav>
+        </div>
+      </header>
+      <main className="min-h-[calc(100vh-57px-97px)] flex-1">
+        <div className="container relative pb-10">
+          <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-6">
+            <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+              Sidebar example built on top of shadcn/ui
+            </h1>
+            <span className="max-w-[750px] text-center text-lg font-light text-foreground">
+              A stunning and functional retractable sidebar for Next.js using
+              shadcn/ui complete with desktop and mobile responsiveness.
+            </span>
+            <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+              <Button variant="default" asChild>
+                <Link href="/dashboard">
+                  Demo
+                  <ArrowRightIcon className="ml-2" />
+                </Link>
+              </Button>
+              <Button variant="outline" asChild>
+                <Link
+                  href="https://ui.shadcn.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn shadcn/ui
+                </Link>
+              </Button>
+            </div>
+          </section>
+          <div className="w-full flex justify-center relative">
+            <Image
+              src="/demo-light-min.png"
+              width={1080}
+              height={608}
+              alt="demo"
+              priority
+              className="border rounded-xl shadow-sm dark:hidden"
+            />
+            <Image
+              src="/demo-dark-min.png"
+              width={1080}
+              height={608}
+              alt="demo-dark"
+              priority
+              className="border border-zinc-600 rounded-xl shadow-sm hidden dark:block dark:shadow-gray-500/5"
+            />
+            <Image
+              src="/demo-mobile-light-min.png"
+              width={228}
+              height={494}
+              alt="demo-mobile"
+              className="border rounded-xl absolute bottom-0 right-0 hidden lg:block dark:hidden"
+            />
+            <Image
+              src="/demo-mobile-dark-min.png"
+              width={228}
+              height={494}
+              alt="demo-mobile"
+              className="border border-zinc-600 rounded-xl absolute bottom-0 right-0 hidden dark:lg:block"
+            />
+          </div>
+        </div>
+      </main>
+      <footer className="py-6 md:py-0 border-t border-border/40">
+        <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
+          <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
+            Built on top of{" "}
+            <Link
+              href="https://ui.shadcn.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              shadcn/ui
+            </Link>
+            . The source code is available on{" "}
+            <Link
+              href="https://github.com/salimi-my/shadcn-ui-sidebar"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              GitHub
+            </Link>
+            .
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}

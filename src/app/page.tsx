d07ff2c03c46cc3551797174f7ce3be8727f47d6
Kartik <kartik20044@gmail.com>
2024-08-15 08:42:06 -0300

    content chaged of default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,8 +87,8 @@ export default function HomePage() {
             className="flex justify-start items-center hover:opacity-85 transition-opacity duration-300"
           >
             <PanelsTopLeft className="w-6 h-6 mr-3" />
-            <span className="font-bold">shadcn/ui sidebar</span>
-            <span className="sr-only">shadcn/ui sidebar</span>
+            <span className="font-bold">PMA</span>
+            <span className="sr-only">PMA</span>
           </Link>
           <nav className="ml-auto flex items-center gap-2">
             <Button
@@ -97,7 +97,7 @@ export default function HomePage() {
               className="rounded-full w-8 h-8 bg-background"
               asChild
             >
-              <Link href="https://github.com/salimi-my/shadcn-ui-sidebar">
+              <Link href="https://github.com/kartikmalik0">
                 <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
               </Link>
             </Button>
@@ -109,20 +109,20 @@ export default function HomePage() {
         <div className="container relative pb-10">
           <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-6">
             <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
-              Sidebar example built on top of shadcn/ui
+              Unlock Your Productivity Today! 
             </h1>
             <span className="max-w-[750px] text-center text-lg font-light text-foreground">
               A stunning and functional retractable sidebar for Next.js using
               shadcn/ui complete with desktop and mobile responsiveness.
             </span>
             <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
-              <Button variant="default" asChild>
+              <Button variant="default" size={"lg"} asChild>
                 <Link href="/dashboard">
-                  Demo
+                   Let's get started
                   <ArrowRightIcon className="ml-2" />
                 </Link>
               </Button>
-              <Button variant="outline" asChild>
+              {/* <Button variant="outline" asChild>
                 <Link
                   href="https://ui.shadcn.com/"
                   target="_blank"
@@ -130,7 +130,7 @@ export default function HomePage() {
                 >
                   Learn shadcn/ui
                 </Link>
-              </Button>
+              </Button> */}
             </div>
           </section>
           <div className="w-full flex justify-center relative">
@@ -181,7 +181,7 @@ export default function HomePage() {
             </Link>
             . The source code is available on{" "}
             <Link
-              href="https://github.com/salimi-my/shadcn-ui-sidebar"
+              href="https://github.com/kartikmalik0"
               target="_blank"
               rel="noopener noreferrer"
               className="font-medium underline underline-offset-4"
